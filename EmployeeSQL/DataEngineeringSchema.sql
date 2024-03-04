07e8071796e3c49ba7473fdcbe6fe9cd12b16a1c
gmmarsh <gmmarsh@gmail.com>
2024-03-04 11:18:30 -0500

    re-ordered table creation to account for foreign keys

--- EmployeeSQL/DataEngineeringSchema.sql
+++ EmployeeSQL/DataEngineeringSchema.sql
@@ -4,6 +4,24 @@ CREATE TABLE department (
     dept_name VARCHAR(100) NOT NULL
 );
 
+--titles table
+CREATE TABLE titles (
+    title_id VARCHAR(100) NOT NULL PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+);
+
+--employee table
+CREATE TABLE employees (
+    emp_no INT NOT NULL PRIMARY KEY,
+    emp_title_id VARCHAR(100) NOT NULL,
+    birth_date DATE NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    sex VARCHAR(1) NOT NULL,
+    hire_date DATE NOT NULL,
+    FOREIGN KEY (emp_title_id) REFERENCES titles (title_id)
+);
+
 --dept_emp table
 CREATE TABLE dept_emp (
     emp_no INT NOT NULL,
@@ -22,28 +40,10 @@ CREATE TABLE dept_manager (
     FOREIGN KEY (dept_no) REFERENCES department (dept_no)
 );
 
---employee table
-CREATE TABLE employees (
-    emp_no INT NOT NULL PRIMARY KEY,
-    emp_title_id VARCHAR(100) NOT NULL,
-    birth_date DATE NOT NULL,
-    first_name VARCHAR(100) NOT NULL,
-    last_name VARCHAR(100) NOT NULL,
-    sex VARCHAR(1) NOT NULL,
-    hire_date DATE NOT NULL,
-    FOREIGN KEY (emp_title_id) REFERENCES titles (title_id)
-);
-
 --salaries table
 CREATE TABLE salaries (
     emp_no INT NOT NULL,
     salary INT NOT NULL,
     PRIMARY KEY (emp_no, salary),
     FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
-);
-
---titles table
-CREATE TABLE titles (
-    title_id VARCHAR(100) NOT NULL PRIMARY KEY,
-    title VARCHAR(100) NOT NULL,
 );
